dc8817f72d61fc17918fc0280a65c5a627a4f43a
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-04-24 07:48:20 +0300

    ain

---
+++ Module-05 = Assignment-01/problem1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello, World! I am Learning C programming language. ^-^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn! \t I will succeed one day.");
+
+    return 0;
+}
---
+++ Module-05 = Assignment-01/problem2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    long long A, B;
+    scanf("%lld %lld", &A, &B);
+    long long multiply = A*B;
+    printf("%lld", multiply);
+
+    return 0; 
+}
