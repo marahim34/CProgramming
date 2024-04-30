aa801529596d482b75810ab8dd43570ddfe45bb4
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-04-30 20:24:23 +0300

    sum of two

---
+++ Module-13/compare_two_values.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            printf("%d %d\n", i, j);
+        }
+    }
+
+    return 0;
+}
