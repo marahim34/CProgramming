a539e0a209bdccbb29d6f74f8929cb2ad85aa0b9
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2024-05-11 08:35:00 +0300

    Guess the data type

--- Practice_11.05/H_Data_Type_Guessing.c
+++ Practice_11.05/H_Data_Type_Guessing.c
@@ -2,8 +2,26 @@
 
 int main()
 {
-    int n, k, a;
-    scanf("%d %d %d", &n, &k, &a);
+    long long n, k, a;
+    scanf("%lld %lld %lld", &n, &k, &a);
 
+    // Using double to preserve any fractional part if that was a requirement
+    double result = (double)n * k / a;
+
+    if (result == (int)result)
+    {
+        if ((int)result >= -2147483648 && (int)result <= 2147483647)
+        {
+            printf("int");
+        }
+        else
+        {
+            printf("long long");
+        }
+    }
+    else
+    {
+        printf("double");
+    }
     return 0;
 }
